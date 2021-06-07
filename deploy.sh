2bffca60cc9d46f75a58713894355594bfe9b5ac
Maurits van der Schee <maurits@vdschee.nl>
2021-06-07 19:01:53 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+#
+# send incremental changes to production server using rsync with flags:
+#
+# -z Zip compress the transfer to reduce bandwidth
+# -r Recursive processing includes subdirectories
+# -c Checksum based comparison as regeneration touches file dates
+# -v Verbose output of all transfered files
+# -h Human readable file sizes
+#
 rm -Rf public && hugo && rsync -zrcvh --delete-delay public/ hugocodexorg@ams01.usecue.nl:public_html/
