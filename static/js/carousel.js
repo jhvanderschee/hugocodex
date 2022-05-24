cecdf8fd81b52aa15b9f77aa4ecd03c597320038
Joost van der Schee <joost@vdschee.nl>
2022-05-24 13:34:39 +0200

    update

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
             
         // Attach the handlers
-        ele.addEventListener("scroll", setSelected);
+        ele.addEventListener("scroll", debounce(setSelected));
         nextarrow.addEventListener("click", nextSlide);
         prevarrow.addEventListener("click", prevSlide);
         bullets.forEach(function(bullet) {
@@ -78,3 +78,30 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 }); //end onload
+
+
+
+
+/**
+ * Debounce functions for better performance
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Function} fn The function to debounce
+ */
+ function debounce (fn) {
+	// Setup a timer
+	let timeout;
+	// Return a function to run debounced
+	return function () {
+		// Setup the arguments
+		let context = this;
+		let args = arguments;
+		// If there's a timer, cancel it
+		if (timeout) {
+			window.cancelAnimationFrame(timeout);
+		}
+		// Setup the new requestAnimationFrame()
+		timeout = window.requestAnimationFrame(function () {
+			fn.apply(context, args);
+		});
+	};
+}
