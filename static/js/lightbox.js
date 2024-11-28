1e4a720133f09ad900d46239732783f3dd05113b
Joost van der Schee <joost@vdscheenl>
2024-11-29 00:06:27 +0100

    update

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -112,7 +112,7 @@ document.addEventListener("DOMContentLoaded", function () {
         event.stopPropagation();
         if (event.target.id != 'next' && event.target.id != 'prev') {
             this.innerHTML = '';
-            document.getElementById('lightbox').style.display = 'none';
+            document.getElementById('lightbox').classList.remove('open');
         }
     });
 
@@ -123,7 +123,7 @@ document.addEventListener("DOMContentLoaded", function () {
             event.preventDefault();
             event.stopPropagation();
             document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="videoWrapperContainer"><div class="videoWrapper"><iframe src="https://www.youtube.com/embed/' + this.getAttribute('data-id') + '?autoplay=1&showinfo=0&rel=0"></iframe></div>';
-            document.getElementById('lightbox').style.display = 'block';
+            document.getElementById('lightbox').classList.add('open');
 
             setGallery(this);
         });
@@ -136,7 +136,7 @@ document.addEventListener("DOMContentLoaded", function () {
             event.preventDefault();
             event.stopPropagation();
             document.getElementById('lightbox').innerHTML = '<a id="close"></a><a id="next">&rsaquo;</a><a id="prev">&lsaquo;</a><div class="img" style="background: url(\'' + this.getAttribute('href') + '\') center center / contain no-repeat;" title="' + this.getAttribute('title') + '" ><img src="' + this.getAttribute('href') + '" alt="' + this.getAttribute('title') + '" /></div><span>' + this.getAttribute('title') + '</span>';
-            document.getElementById('lightbox').style.display = 'block';
+            document.getElementById('lightbox').classList.add('open');
 
             setGallery(this);
         });
